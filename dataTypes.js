62447fdc78e08f737b6ba0d567c67597a57aa858
Usman Aftab <dev.usmanaftab@gmail.com>
2022-02-09 21:27:43 +0500

    editing

--- dataTypes.js
+++ dataTypes.js
@@ -1,3 +1,9 @@
-// Data Types: Boolean, Number, String, Symbol, Object, Null, Undefined. 
+// Data Types: boolean, number, string, symbol, object, null, undefined. 
 
-var 
+const pi = 3.14;
+var num1 = 10;
+let num2 = 20;
+var isEven = true;
+var myName = "Usman Aftab";
+var emptyVariable  = null;
+var unInitialized  = undefined;
