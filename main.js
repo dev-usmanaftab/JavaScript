5dcbff468e26fac62b98943545051165bf8bb789
Usman Aftab <dev.usmanaftab@gmail.com>
2022-02-09 23:44:31 +0500

    edit

--- main.js
+++ main.js
@@ -2,5 +2,6 @@
    let is used to declare those variable which we want to use in a Specific Scope.
    Const is used to declare those variable which will remain Constant in whole program. */
 
-   let age = prompt("What is your age?", 30);
+   let age = prompt("What is your age?", 18);
+   alert(`Your are is ${age}`);
    
