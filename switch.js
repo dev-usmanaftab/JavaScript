e811693c03c4a81a3edf241c34ad5d1e31aa7064
Usman Aftab <dev.usmanaftab@gmail.com>
2022-02-11 18:27:03 +0500

    correction in switch

--- switch.js
+++ switch.js
@@ -1,20 +1,20 @@
-var result = 75;
+var result = 70;
 
 switch (result) {
-    case result>=50 && result<60:
-        console.log('Your Grade is D ');
+    case 90:
+        console.log('Your Grade is A+');
         break;
-    case result>=60 && result<70:
-        console.log('Your Grade is C ');
+    case 80:
+        console.log('Your Grade is A');
             break;
-    case result>=70 && result<80:
+    case 70:
         console.log('Your Grade is B ');
             break;
-    case result>=80 && result<90:
-        console.log('Your Grade is A ');
+    case 60:
+        console.log('Your Grade is C ');
             break;
-    case result>=90 && result<101:
-        console.log('Your Grade is A+');
+    case 50:
+        console.log('Your Grade is D');
             break;
     default:
         console.log('Your Grade is F ');
