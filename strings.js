4c3ef3fa245b90afb5f552a04f82ed76041f23ea
Usman Aftab <dev.usmanaftab@gmail.com>
2022-02-12 01:39:06 +0500

    edited

--- strings.js
+++ strings.js
@@ -1,6 +1,24 @@
-var str1 = "Hello World";
-var srt2 = 'Matrix Reloaded';
-var str3 = `the quick brown fox jumps over the lazy dog`;
-
-var length = String.length(str3);
+var str = `Hello World`;
+var length = str.length;
 console.log(length);
+
+/*-----------------------------------------------------------*/
+// String Concatenation
+
+var str1 = "Matrix ";
+var str2 = str1.concat("Reloaded");
+console.log(str2);
+
+/*-----------------------------------------------------------*/
+// index of a element
+
+str = `the quick brown fox jumps over the lazy dog`;
+var index = str.indexOf('q');
+console.log(index);
+
+/*-----------------------------------------------------------*/
+// Trim (remove white space from start and end)
+
+var email     = "     usman@gmail.com     "
+var trim_demo = email.trim();
+console.log(trim_demo);
