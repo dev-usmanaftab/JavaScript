bc810851684782914e5527cc0fb035761acc67d0
Usman Aftab <dev.usmanaftab@gmail.com>
2022-06-26 12:59:32 +0500

    practice

---
+++ practice.js
@@ -0,0 +1,12 @@
+var a = {
+            firstName: 'John',
+            lastName:  'Smith',
+            Age:        25,
+};
+
+console.log("My first Name: ",a.firstName );
+console.log("My Last Name: ",a.lastName );
+console.log("My Age is: ",a.Age);
+
+
+
